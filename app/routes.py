a6e725094fc4554dfb7204d5e6656c31c0aa1987
Ucl.org.ru <vauglev@gmail.com>
2021-05-26 18:33:28 +0700

    Update routes.py

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,47 @@
 # -*- coding: utf-8 -*-
-from app import app
+from app   import app
+from app   import collection
+from flask import request, jsonify
 
-@app.route('/')
-@app.route('/index')
+@app.route('/upsl')
+def index(): # начальная меню панель 
+    return "Hello, World!"
+
+@app.route('/upsl/adday ')
+def index(): # хама форма добавления дня
+    return "Hello, World!"
+
+@app.route('/upsl/api/adday, methods=['GET', 'POST'] ')
+def index(): # сюда json 
+    content = request.json
+    return "Hello, World!"
+
+
+
+@app.route('/upsl/liday')
+def index():
+    return "Hello, World!"
+
+@app.route('/upsl/sutr')
+def index(): # ajhvf ryjgrf dhtvz lf форма время дата - редирект на sshow
+    return "Hello, World!"
+
+@app.route('/upsl/api/sutr' , methods=['GET', 'POST'] ')
+def index(): # сюда json 
+    content = request.json
+    return "Hello, World!"
+
+
+
+@app.route('/upsl/slep')
+def index():
+    return "Hello, World!"
+
+@app.route('/upsl/api/slep' , methods=['GET', 'POST'] ')
+def index(): # сюда json 
+    content = request.json
+    return "Hello, World!"
+
+@app.route('/upsl/sush')
 def index():
     return "Hello, World!"
