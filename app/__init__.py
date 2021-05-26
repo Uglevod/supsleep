a378f01735979500cfc0de29eeb037045e72bc86
Ucl.org.ru <vauglev@gmail.com>
2021-05-26 18:15:54 +0700

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -3,3 +3,14 @@ from flask import Flask
 app = Flask(__name__)
 
 from app import routes
+
+from pymongo import MongoClient
+
+# Create the client
+client = MongoClient('localhost', 27017)
+
+# Connect to our database
+db = client.supsl
+
+# Fetch our series collection
+collection = db.ddt
